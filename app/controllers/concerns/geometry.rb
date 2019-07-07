0dd51955ebacf39d9bbb6b6a9c5ba50b6d56cf7d
Hossein Shafiei <h.shafiei@ut.ac.ir>
2019-07-07 11:37:06 +0430

    1

--- app/controllers/concerns/geometry.rb
+++ app/controllers/concerns/geometry.rb
@@ -7,8 +7,8 @@ module Geometry
     regions = RGeo::GeoJSON.decode(area.geo_json)
     point = RGeo::GeoJSON.decode(point_param.to_json)
     @flag = false
-    for region in @regions
-      if region.geometry.contains?(@point.geometry)
+    for region in regions
+      if region.geometry.contains?(point.geometry)
         @flag =  true
       end
     end
